889733aae2892bf2278f357e1fcbbe376612089b
Ben Thompson <smoov.b@gmail.com>
2017-03-31 12:33:30 -0500

    some interim steps to get test ngx-datatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'wui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wui works!';
+
+  // testData = {
+  //   cols: [
+  //     { name: 'name' },
+  //     { name: 'gender' },
+  //     { name: 'age' }
+  //   ],
+  //   rows: [
+  //     { name: 'julie', gender: 'female', weight: '143' },
+  //     { name: 'max', gender: 'male', weight: '176' },
+  //     { name: 'charles', gender: 'male', weight: '203' },
+  //     { name: 'elle', gender: 'female', weight: '168' },
+  //   ]
+  // };
+
+  dataset: any;
+
+  constructor (private _http: Http) { }
+
+  ngOnInit() {
+    this._http.get('assets/data/100k.json')
+      .subscribe(res => {
+        this.dataset = res.json();
+        console.log(this.dataset);
+      });
+  }
 }
