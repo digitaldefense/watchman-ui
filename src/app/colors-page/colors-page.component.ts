4500e9dc6b57f68515863ecd19e292aa50930697
Ben Thompson <smoov.b@gmail.com>
2017-04-20 07:52:27 -0500

    add colors page

---
+++ src/app/colors-page/colors-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wui-colors-page',
+  templateUrl: './colors-page.component.html',
+  styleUrls: ['./colors-page.component.scss']
+})
+export class ColorsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
