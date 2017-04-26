dd7c43df99c55d187ca24235f74c4c6b6af00674
Ben Thompson <smoov.b@gmail.com>
2017-04-26 14:21:34 -0500

    added accent variants

--- src/app/wui/theme/theme.tmpl.ts
+++ src/app/wui/theme/theme.tmpl.ts
@@ -2,6 +2,8 @@ export interface Theme {
   _name: string;
   primary: string;
   accent: string;
+  accentLight: string;
+  accentDark: string;
   success: string;
   warning: string;
   danger: string;
