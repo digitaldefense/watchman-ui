b002c2f28a80d4c83d8ab6db66bd71c429ac84be
Ben Thompson <smoov.b@gmail.com>
2017-04-28 22:24:32 -0500

    commited without saving :/

--- src/app/avatar-page/avatar-page.component.ts
+++ src/app/avatar-page/avatar-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'wui-avatar-page',
   templateUrl: './avatar-page.component.html',
-  styleUrls: ['./avatar-page.component.scss']
 })
 export class AvatarPageComponent implements OnInit {
 
