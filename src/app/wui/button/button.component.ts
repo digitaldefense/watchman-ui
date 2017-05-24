4f3f65ce04cf1d30d251af707b80774e0aa2321a
Ben Thompson <smoov.b@gmail.com>
2017-05-24 10:56:21 -0500

    trying to add hover states to buttons

--- src/app/wui/button/button.component.ts
+++ src/app/wui/button/button.component.ts
@@ -1,9 +1,11 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   Directive,
   ElementRef,
   HostBinding,
+  HostListener,
   Input,
   OnInit,
   Renderer2,
@@ -12,6 +14,7 @@ import {
 
 import { coerceBooleanProperty, /*FocusOriginMonitor*/ } from '../core';
 import { FlThemeService } from '../theme2/theme.service';
+// import { PALETTE } from '../theme2/palette';
 import { Theme } from '../theme/theme.tmpl';
 
 @Directive({
@@ -40,9 +43,10 @@ export class FlIconButtonStyle {}
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FlButtonComponent implements OnInit {
+export class FlButtonComponent implements OnInit, AfterViewInit {
   private _color: string;
   private _disabled: boolean = null;
+  private _isTextBtn: boolean;
 
   @Input()
   get disabled() { return this._disabled; }
@@ -52,6 +56,22 @@ export class FlButtonComponent implements OnInit {
   get color() { return this._color; }
   set color(value: string) { this._color = value; }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    if (this._isTextBtn) {
+      this._themeSvc.applyColor(this._element, this._renderer, 'hover');
+    } else {
+      this._themeSvc.applyBgColor(this._element, this._renderer, 'hover');
+    }
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    if (this._isTextBtn) {
+      this._themeSvc.applyColor(this._element, this._renderer, this.color);
+    } else {
+      this._themeSvc.applyBgColor(this._element, this._renderer, this.color);
+    }
+  }
+
   constructor(
     private _element: ElementRef,
     private _renderer: Renderer2,
@@ -59,15 +79,39 @@ export class FlButtonComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    if (this._disabled) { return; }
-
-    if (this.color == null) { this.color = 'primary'; }
     const elem = this._element.nativeElement;
     const attrs = elem.attributes;
+
+    if (this._disabled || this._color == null) { return; }
+
     if (attrs.hasOwnProperty('fl-button') || attrs.hasOwnProperty('fl-icon-button')) {
       this._themeSvc.applyColor(this._element, this._renderer, this.color);
     } else {
       this._themeSvc.applyBgColor(this._element, this._renderer, this.color);
     }
+  }
+
+  ngAfterViewInit() {
+    const elem = this._element.nativeElement;
+
+    // get the original color for buttons with no color attribute
+    if (this._color == null) {
+      const styles = window.getComputedStyle(elem, null);
+      const bgColor = styles.backgroundColor;
+      this.color = this._rgbToHex(bgColor);
+      console.log(this.color);
+    }
+  }
+
+  private _rgbToHex(value: string) {
+    const SPACE = /\s/g;
+    const rgbStr: string = value.slice(4, value.length - 1).replace(SPACE, '');
+    const rgb: string[] = rgbStr.split(',');
+    return '#' + this._numberToHex(Number(rgb[0])) + this._numberToHex(Number(rgb[1])) + this._numberToHex(Number(rgb[2]));
+  }
+
+  private _numberToHex(c: number): string {
+    const hex = c.toString(16);
+    return (hex.length === 1) ? '0' + hex : hex;
   }
 }
